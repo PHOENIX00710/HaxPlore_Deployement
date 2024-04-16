a9cd9d41504a6e1ba1d97c70444df36c881296c2
Swapnil Jha <swapnil.strange0707@gmail.com>
2024-04-16 20:13:15 +0530

    Socket connection for frontend made to the right url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider=({children})=>{
 
     useEffect(()=>{
         if(authUser){
-            const socket=io('http://localhost:3000');
+            const socket=io('https://haxplore-deployement.onrender.com');
             setSocket(socket)
             return ()=>{
                 socket.close()
